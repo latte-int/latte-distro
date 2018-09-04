4946d47ba8f58fe2ae34413c43d24d88fd9f641c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-09-04 02:32:57 -0700

    .docker-build.sh: Install m4, needed for building gmp

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
-yum install -y gcc gcc-c++ || ( apt-get update && apt-get -y install g++ make)
+yum install -y gcc gcc-c++ || ( apt-get update && apt-get -y install g++ make m4)
 mkdir -p /build
 cd /build
 /src/configure --srcdir=/src  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
